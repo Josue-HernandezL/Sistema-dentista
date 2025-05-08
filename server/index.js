30dd23789dde6852fb4f8af2f4ced8648c8b4b6d
Josue-hernandez <97258271+Josue-HernandezL@users.noreply.github.com>
2025-05-08 04:32:37 -0600

    Desarrollo del Login

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, '..', 'public', 'views', 'login.html');
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error('Error al enviar el archivo:', err);
+            res.status(404).send('Archivo no encontrado');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
