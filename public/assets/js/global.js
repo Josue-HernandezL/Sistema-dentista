0564b9f70dcf22f7091ceee782628816c1c0fe61
Josue-hernandez <97258271+Josue-HernandezL@users.noreply.github.com>
2025-05-11 16:31:14 -0600

    Slide bars completada, y redirrecciones realizadas

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -3,3 +3,21 @@ $index.addEventListener('click', (e) => {
     e.preventDefault();
     window.location.href = '/principal';
 });
+
+document.querySelectorAll('a[class^="redirect"]').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Extraer clase completa, ejemplo: "redirectcitas"
+    const fullClass = element.className;
+
+    // Obtener la parte después de "redirect"
+    const path = fullClass.replace("redirect", "");
+
+    // Redirigir a la URL correspondiente
+    if (path) {
+      window.location.href = `/${path}`;
+    }
+  });
+});
+console.log('Script global.js cargado correctamente');
