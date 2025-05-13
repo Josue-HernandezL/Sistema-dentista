a5078a6df08424d5f86a7aa96b1f6878fcc43422
Josue-hernandez <97258271+Josue-HernandezL@users.noreply.github.com>
2025-05-13 05:20:27 -0600

    Cambios y refactorizacion de carpetas

---
+++ public/assets/js/firebase-config.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
+const DATABASEURL = process.env.DATABASEURL;
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
+const MEASUREMENT_ID = process.env.MEASUREMENT_ID;
+
+const firebaseConfig = {
+    apiKey : FIREBASE_API_KEY,
+    authDomain : FIREBASE_AUTH_DOMAIN,
+    databaseURL : DATABASEURL,
+    projectId : FIREBASE_PROJECT_ID,
+    storageBucket : FIREBASE_STORAGE_BUCKET,
+    messagingSenderId : FIREBASE_MESSAGING_SENDER_ID,
+    appId : FIREBASE_APP_ID,
+    measurementId : MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
