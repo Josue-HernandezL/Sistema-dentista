feffbd18b303f4165ea84142b3196dc80ec96516
Josue-hernandez <97258271+Josue-HernandezL@users.noreply.github.com>
2025-05-13 17:13:41 -0600

    Vista creada de pacientes

--- public/assets/js/pacientes.js
+++ public/assets/js/pacientes.js
@@ -1,9 +1,15 @@
-const modal = document.getElementById('modalNuevoPaciente');
-  const abrir = document.getElementById('btnNuevoPaciente');
-  const cerrar = document.getElementById('cerrarModal');
+ const modal = document.getElementById('ventanaFlotante');
+  const abrir = document.getElementById('openModal');
+  const cerrar = document.getElementById('cerrarVentana');
 
   abrir.onclick = () => modal.style.display = 'flex';
   cerrar.onclick = () => modal.style.display = 'none';
   window.onclick = (e) => { if (e.target === modal) modal.style.display = 'none'; };
 
-  
+  document.getElementById('formularioEjemplo').addEventListener('submit', function(e) {
+    e.preventDefault(); // evita recarga
+    const datos = Object.fromEntries(new FormData(this));
+    console.log('📤 Datos enviados:', datos);
+    modal.style.display = 'none'; // cerrar modal
+    this.reset(); // limpiar formulario
+  });
