46eb897b91407f3a96563184b6d86cd1b0076632
Josue-hernandez <97258271+Josue-HernandezL@users.noreply.github.com>
2026-06-04 20:02:55 -0600

    Actualización de la configuración de Firebase

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey : "AIzaSyDN7GPLrDi-Z5yScuuF0MThITDaFJt_kXE",
-    authDomain : "ortodoncista-system-5c89c.firebaseapp.com",
-    databaseURL : "https://ortodoncista-system-5c89c-default-rtdb.firebaseio.com",
-    projectId : "ortodoncista-system-5c89c",
-    storageBucket : "ortodoncista-system-5c89c.firebasestorage.app",
-    messagingSenderId : "474809403013",
-    appId : "1:474809403013:web:f718367589809fc8864bec",
-    measurementId : "G-MBE5NH8VE0"
+    apiKey: "AIzaSyB6wRvkBeGTWc_gP0nKhzDMpMxv_gdyUNI",
+    authDomain: "dente-ecc96.firebaseapp.com",
+    databaseURL : "https://dente-ecc96-default-rtdb.firebaseio.com/",
+    projectId: "dente-ecc96",
+    storageBucket: "dente-ecc96.firebasestorage.app",
+    messagingSenderId: "383729656131",
+    appId: "1:383729656131:web:d1e977ba9176d7a18ccff6",
+    measurementId: "G-P6RBX7WT09"
 };
 
 export const app = initializeApp(firebaseConfig);
